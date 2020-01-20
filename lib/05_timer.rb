8b8833c147c51ed704e2c5676606d67d4df06d76
lifeae <romain.leporatti@laposte.net>
2020-01-20 20:08:10 +0100

    05 work ! fock yeaa !

--- lib/05_timer.rb
+++ lib/05_timer.rb
@@ -1,15 +1,27 @@
 def time_string(seconds)
   seconds = seconds.to_i
-  if seconds >= 60
+
+  if seconds < 10
+    return "00:00:0#{seconds}"
+  elsif seconds < 60
+    return "00:00:#{seconds}"
+  else seconds >= 60
     minutes = seconds / 60
-    return "#{heures}:#{minutes}:#{seconds}"
-      if minutes >= 60
-        heures = minutes / 60
+    seconds = seconds-(minutes*60)
+    if minutes < 10 and seconds < 10
+      return "00:0#{minutes}:0#{seconds}"
+    elsif minutes < 10
+      return "00:0#{minutes}:#{seconds}"
+    elsif minutes < 60
+      return "00:#{minutes}:#{seconds}"
+    else minutes >= 60
+      heures = minutes / 60
+      minutes = minutes-(heures*60)
+      if heures < 10 and minutes < 10
+        return "0#{heures}:0#{minutes}:#{seconds}"
+      else
         return "#{heures}:#{minutes}:#{seconds}"
       end
-return "#{heures}:#{minutes}:#{seconds}"
-  else
-    return "00:00:#{seconds}"
-      
+    end
   end
 end
